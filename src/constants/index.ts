059acb48d7ef304720486789dee86431cd72c81a
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-12-23 22:16:51 +0500

    project init

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export enum ProductTrackingType {
+  SIMPLE = 'SIMPLE',
+  EXPIRABLE = 'EXPIRABLE',
+  SERIALIZED = 'SERIALIZED',
+  LOT_TRACKED = 'LOT_TRACKED',
+  VARIANT = 'VARIANT',
+}
+
---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.enableCors({
+    origin: '*',
+    // credentials: true
+  });
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Itech Quiz Up API')
+    .setDescription('API documentation for the Itech Quiz Up platform')
+    .setVersion('1.0')
+    .addTag('auth', 'Authentication endpoints')
+    .addTag('users', 'User management endpoints')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  const port = await configService.get('PORT');
+
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+bootstrap();
